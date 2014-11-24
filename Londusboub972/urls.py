0169d5520000be757a423b7d0ab577a4e4b276fb
Simplonco <you@example.com>
2014-11-24 12:34:29 +0100

    Testing templates, remove useless br/ in views.

--- Londusboub972/urls.py
+++ Londusboub972/urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.shortcuts import render
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'Londusboub972.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^affichage$', 'Affichage'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'blog.views.home'),
+
 )
